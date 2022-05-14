8b2e34940bae2b648c39a27a79a6cd39aa60dd72
zeynepgok <zeynep.gk3@gmail.com>
2022-05-14 20:35:55 +0300

    created homepage and login section

--- js/components.js
+++ js/components.js
@@ -1,40 +1,25 @@
 class MyHeader extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML =
-            `
+	connectedCallback() {
+		this.innerHTML =
+			`
         <div class="bg-top navbar-light">
 		<div class="container">
 			<div class="row no-gutters d-flex align-items-center align-items-stretch">
 				<div class="col-md-4 d-flex align-items-center py-4">
 					<a class="navbar-brand" href="index.html">Unitopia <span>Utopian University</span></a>
 				</div>
-				<div class="col-lg-8 d-block">
-					<div class="row d-flex">
-						<div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
-							<div class="icon d-flex justify-content-center align-items-center"><span
-									class="icon-paper-plane"></span></div>
-							<div class="text">
-								<span>Email</span>
-								<span>youremail@email.com</span>
+				<div class="col-lg-8 d-flex align-items-center py-4" style="justify-content:right">
+						<form class="form-inline">
+							<div class="form-group mb-2">
+								<label for="exampleFormControlInput1"> </label>
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email Adresiniz">
 							</div>
-						</div>
-						<div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
-							<div class="icon d-flex justify-content-center align-items-center"><span
-									class="icon-phone2"></span></div>
-							<div class="text">
-								<span>Call</span>
-								<span>Call Us: + 1235 2355 98</span>
+							<div class="form-group mx-sm-3 mb-2">
+								<label for="inputPassword2" class="sr-only">parola</label>
+								<input type="password" class="form-control" id="inputPassword2" placeholder="Parola">
 							</div>
-						</div>
-						<div class="col-md topper d-flex align-items-center justify-content-end">
-							<p class="mb-0">
-								<a href="#"
-									class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
-									<span>Giriş Yap</span>
-								</a>
-							</p>
-						</div>
-					</div>
+							<button type="submit" class="btn btn-primary mb-2">Giriş Yap</button>
+						</form>
 				</div>
 			</div>
 		</div>
@@ -120,16 +105,16 @@ class MyHeader extends HTMLElement {
 		</div>
 	</nav>
         `
-    }
+	}
 }
 customElements.define('my-header', MyHeader)
 
 /*-----------*/
 
 class MyFooter extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML =
-            `
+	connectedCallback() {
+		this.innerHTML =
+			`
             <footer class="ftco-footer ftco-bg-dark ftco-section">
             <div class="container">
               <div class="row mb-5">
@@ -215,6 +200,6 @@ class MyFooter extends HTMLElement {
             </div>
           </footer>
         `
-    }
+	}
 }
 customElements.define('my-footer', MyFooter)
