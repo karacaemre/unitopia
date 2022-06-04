3185fb757d24cf3e1ea3ef2efe675f03ad855b8e
Emre <emre.karaca@tuvimer.com>
2022-06-05 02:33:59 +0300

    blogs images added

--- js/app.js
+++ js/app.js
@@ -25,12 +25,20 @@ const loadCharacters = async () => {
     }
 };
 
+
 const displayCharacters = (characters) => {
     const htmlString = characters
         .map((character) => {
             return `
-            <div  class="col-md-6 col-lg-4">
+            <div class="col-md-6 col-lg-4">
             <div class="blog-entry">
+            <a href="blog-single.html" class="block-20 d-flex align-items-end" style="background-image: url('images/blog_ses.webp');">
+                <div class="meta-date text-center p-2">
+                    <span class="day">26</span>
+                    <span class="mos">Mayıs</span>
+                    <span class="yr">2019</span>
+                </div>
+            </a>
                 <div class="text bg-white p-4">
                     <h3 class="heading">${character.header}</h3>
                     <p>${character.content}</p>
