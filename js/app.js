bb255c78c1912c9855551c2d61dad7d5b768b29b
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2022-06-05 01:16:05 +0300

    blog liste

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const charactersList = document.getElementById('charactersList');
+const searchBar = document.getElementById('searchBar');
+let hpCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.header.toLowerCase().includes(searchString) ||
+            character.content.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('http://localhost:3001/announcements/getAll');
+        console.log(res);
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.header}</h2>
+                <h5>${character.authorId}</h5>
+                <p>${character.content}</p>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
